a92fc90d79978ab65ac7aea906acb49eba379ca3
Krishna Kondapalli <srikrishnakvs472@gmail.com>
2020-03-08 12:25:11 +0530

    Updates

---
+++ precache-manifest.8e52594f7c84aeee37235d193e163b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3333d527d228dd2eab8441d783c78f99",
+    "url": "/app_08/index.html"
+  },
+  {
+    "revision": "ff4369a2926739c5daa7",
+    "url": "/app_08/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c861073abdb6ef2b3598",
+    "url": "/app_08/static/js/2.a576e421.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/app_08/static/js/2.a576e421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4369a2926739c5daa7",
+    "url": "/app_08/static/js/main.fe4cfb15.chunk.js"
+  },
+  {
+    "revision": "975130f170576b98f07e",
+    "url": "/app_08/static/js/runtime-main.e76c8ad7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app_08/static/media/logo.5d5d9eef.svg"
+  }
+]);
